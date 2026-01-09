36b18d5a80aa9a4e22d19dae1e0049d71a51b776
queueRAM <queueRAM@proton.me>
2026-01-08 23:24:19 -0800

    Decompile some memory functions (#5)
    - Decompile _uvMemGetScratch
    - Decompile _uvMemFreeScratch
    - Decompile _uvJumpHeap
    - Decompile uvSysInit with fakematch

--- include/uv_memory.h
+++ include/uv_memory.h
@@ -7,8 +7,14 @@
 void _uvMediaCopy(void* vAddr, void* devAddr, u32 nbytes);
 void _uvDMA(void* vAddr, u32 devAddr, u32 nbytes);
 
+void uvSysInit(void);
+
+s32 _uvJumpHeap(u32* arg0);
+
 u64 uvMemRead(void* vAddr, u32 nbytes);
 void uvMemSet(void* vAddr, u8 value, u32 nbytes);
 
-#endif // PILOTWINGS64_UV_MEMORY
+void _uvMemFreeScratch(void *addr);
+void *_uvMemGetScratch(u32 size);
 
+#endif // PILOTWINGS64_UV_MEMORY
