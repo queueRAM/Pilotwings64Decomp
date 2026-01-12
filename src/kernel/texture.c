cb44396a644f98b7d01e31fbe293cbeb5a53a184
queueRAM <queueram@proton.me>
2026-01-11 22:17:01 -0800

    Decompile func_80227804

--- src/kernel/texture.c
+++ src/kernel/texture.c
@@ -1,12 +1,14 @@
 #include "common.h"
 #include <uv_graphics.h>
 
-void *func_802256B8(void*);
-void *func_80225470(void*);
-void *func_802254B0(void*);
-void *_uvExpandTextureCpy(void*);
+void* func_802256B8(void*);
+void* func_80225470(void*);
+void* func_802254B0(void*);
+void* func_80225FBC(void*);
+void* _uvExpandTextureCpy(void*);
 
 extern u32 D_802B53F4[];
+extern u32 D_802B5A34[];
 extern u32 D_802B6404;
 extern u32 D_802B6484;
 extern u32 D_802B64E4;
@@ -43,7 +45,25 @@ extern u32 D_802B64E4;
 
 #pragma GLOBAL_ASM("asm/nonmatchings/kernel/texture/func_80227260.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/kernel/texture/func_80227804.s")
+//#pragma GLOBAL_ASM("asm/nonmatchings/kernel/texture/func_80227804.s")
+void* func_80227804(s32 arg0) {
+    s32 idx;
+    s32 tag;
+    u32 sp3C;
+    void* sp38;
+    void* ret;
+
+    ret = NULL;
+    idx = func_80223E80(D_802B5A34[arg0]);
+    while ((tag = func_80223F7C(idx, &sp3C, &sp38, 1)) != 0) {
+        if (tag == 'COMM') { // 0x434F4D4D
+            if (1) {} // fakematch
+            ret = func_80225FBC(sp38);
+        }
+    }
+    func_80223F30(idx);
+    return ret;
+}
 
 //#pragma GLOBAL_ASM("asm/nonmatchings/kernel/texture/func_802278C0.s")
 void* func_802278C0(s32 arg0) {
@@ -62,13 +82,13 @@ void* func_802278C0(s32 arg0) {
 }
 
 //#pragma GLOBAL_ASM("asm/nonmatchings/kernel/texture/func_80227938.s")
-void *func_80227938(s32 arg0) {
+void* func_80227938(s32 arg0) {
     s32 idx;
     s32 sp28;
-    void *sp24;
-    void *ret;
+    void* sp24;
+    void* ret;
 
-    ret = 0;
+    ret = NULL;
     idx = func_80223E80(D_802B64E4);
     if (func_80224170(idx, &sp28, &sp24, 'COMM', arg0, 1) != 0) { // 0x434F4D4D
         ret = _uvExpandTextureCpy(sp24);
@@ -80,13 +100,13 @@ void *func_80227938(s32 arg0) {
 #pragma GLOBAL_ASM("asm/nonmatchings/kernel/texture/func_802279B0.s")
 
 //#pragma GLOBAL_ASM("asm/nonmatchings/kernel/texture/func_80227A28.s")
-s32 func_80227A28(s32 arg0) {
+void* func_80227A28(s32 arg0) {
     s32 sp2C;
     s32 sp28;
-    void *sp24;
-    s32 ret;
+    void* sp24;
+    void* ret;
 
-    ret = 0;
+    ret = NULL;
     sp2C = func_80223E80(D_802B6484);
     if (func_80224170(sp2C, &sp28, &sp24, 'COMM', arg0, 1) != 0) { // 0x434F4D4D
         ret = func_80225470(sp24);
@@ -96,22 +116,22 @@ s32 func_80227A28(s32 arg0) {
 }
 
 //#pragma GLOBAL_ASM("asm/nonmatchings/kernel/texture/func_80227AA0.s")
-void *func_80227AA0(s32 arg0) {
-    s32 temp_v0;
+void* func_80227AA0(s32 arg0) {
+    s32 idx;
     s32 tag;
     u32 sp3C;
     void* sp38;
     void* ret;
 
     ret = NULL;
-    temp_v0 = func_80223E80(D_802B53F4[arg0]);
-    while ((tag = func_80223F7C(temp_v0, &sp3C, &sp38, 1)) != 0) {
+    idx = func_80223E80(D_802B53F4[arg0]);
+    while ((tag = func_80223F7C(idx, &sp3C, &sp38, 1)) != 0) {
         if (tag == 'COMM') { // 0x434F4D4D
             if (1) {} // fakematch
             ret = func_802256B8(sp38);
         }
     }
-    func_80223F30(temp_v0);
+    func_80223F30(idx);
     return ret;
 }
 
