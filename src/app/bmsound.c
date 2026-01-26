7d00d606c39eba1248ebc60ed85db4e636de9389
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-26 01:45:49 -0800

    rename bmsound.c

---
+++ src/app/bmsound.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/bmsound/func_802D0BF0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/bmsound/func_802D0D04.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/bmsound/func_802D112C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/bmsound/func_802D12C4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/bmsound/func_802D1320.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/bmsound/func_802D1334.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/bmsound/func_802D1534.s")
