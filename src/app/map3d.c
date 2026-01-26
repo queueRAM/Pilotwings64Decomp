b9611247c2b30fb45748f81c2468045bfdc619d7
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-26 00:17:47 -0800

    rename map3d.c

---
+++ src/app/map3d.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/map3d/app_entrypoint.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/map3d/func_8030FE80.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/map3d/map3d.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/map3d/func_80311660.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/map3d/func_80311C68.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/map3d/func_803122B4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/map3d/func_803123C4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/map3d/func_80312514.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/map3d/map3d_mslot.s")
