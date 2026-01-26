4e502c7d84f93ed5d92b645b455e49c87f345b60
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-25 19:40:48 -0800

    rodata

---
+++ src/app/code_D1ED0.c
@@ -0,0 +1,41 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034A9A0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034A9E8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034AA80.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034AACC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034ABB0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034AC34.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034AD14.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034AD6C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034AF48.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034AF94.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B210.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B268.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B2B0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B354.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B464.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B4DC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B5E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B624.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B688.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D1ED0/func_8034B6F8.s")
---
+++ src/app/code_D2D50.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034B820.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034B82C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034BA68.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034BC68.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034BDCC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034BEDC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034C0BC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034C154.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034C16C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034C1CC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034C224.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034C25C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_D2D50/func_8034C298.s")
