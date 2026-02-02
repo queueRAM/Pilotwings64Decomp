528ba3985c42aa25712ddca7bd9203c0d82f0f3b
queueRAM <queueram@proton.me>
2026-02-01 18:19:09 -0800

    WIP on func_8022A27C

--- src/kernel/code_2AE50.c
+++ src/kernel/code_2AE50.c
@@ -17,8 +17,54 @@ void Length3D(f32 x, f32 y, f32 z) {
     SqrtF(SQ(x) + SQ(y) + SQ(z));
 }
 
-f32 func_8022A27C(f32, f32);
+#ifndef NON_MATCHING
 #pragma GLOBAL_ASM("asm/nonmatchings/kernel/code_2AE50/func_8022A27C.s")
+#else
+#define PW_PI   3.1415927f
+#define PW_PI_2 1.5707963f
+#define PW_PI_4 0.7853982f
+
+f32 func_8022A27C(f32 arg0, f32 arg1) {
+    s32 flag;
+    f32 ret;
+    f32 pi2;
+    f32 fabsA0;
+    f32 fabsA1;
+    f32 ratio; // sp4;
+    f32 absdiff; // sp8;
+    f32 temp_ft5;
+
+    if ((arg1 == 0.0f) && (arg0 == 0.0f)) {
+        return 0.0f;
+    }
+    fabsA1 = FABS(arg1);
+    fabsA0 = FABS(arg0);
+    flag = 0;
+    if (fabsA1 < fabsA0) {
+        temp_ft5 = fabsA1;
+        fabsA1 = fabsA0;
+        fabsA0 = temp_ft5;
+        flag = 1;
+    }
+    ratio = (fabsA0 / fabsA1);
+    absdiff = FABS(fabsA0 - fabsA1);
+    ret = (ratio * PW_PI_4) + (0.309f * absdiff * ratio);
+    pi2 = PW_PI_2;
+    if (arg1 >= 0.0f) {
+        if (flag) {
+            ret = pi2 - ret;
+        }
+    } else if (flag) {
+        ret = pi2 + ret;
+    } else {
+        ret = PW_PI - ret;
+    }
+    if (arg0 >= 0.0f) {
+        return ret;
+    }
+    return -ret;
+}
+#endif
 
 #pragma GLOBAL_ASM("asm/nonmatchings/kernel/code_2AE50/D_8024E3A0.s")
 
