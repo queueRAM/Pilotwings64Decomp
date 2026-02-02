4f226a3fb9f7b2396b4abb33026f6df1b7815cd5
queueRAM <queueram@proton.me>
2026-02-02 10:41:59 -0800

    Decompile func_8022A27C and add math_macros.h

---
+++ include/math_macros.h
@@ -0,0 +1,8 @@
+#ifndef MATH_MACROS_H
+#define MATH_MACROS_H
+
+#define PW_PI   3.1415927f
+#define PW_PI_2 1.5707963f // PI/2
+#define PW_PI_4 0.7853982f // PI/4
+
+#endif // MATH_MACROS_H
--- src/kernel/math.c
+++ src/kernel/math.c
@@ -16,22 +16,13 @@ void uvLength3D(f32 x, f32 y, f32 z) {
     uvSqrtF(SQ(x) + SQ(y) + SQ(z));
 }
 
-#ifndef NON_MATCHING
-#pragma GLOBAL_ASM("asm/nonmatchings/kernel/math/func_8022A27C.s")
-#else
-#define PW_PI   3.1415927f
-#define PW_PI_2 1.5707963f
-#define PW_PI_4 0.7853982f
-
 f32 func_8022A27C(f32 arg0, f32 arg1) {
     s32 flag;
     f32 ret;
-    f32 pi2;
     f32 fabsA0;
     f32 fabsA1;
-    f32 ratio; // sp4;
-    f32 absdiff; // sp8;
     f32 temp_ft5;
+    f32 absdiff;
 
     if ((arg1 == 0.0f) && (arg0 == 0.0f)) {
         return 0.0f;
@@ -45,23 +36,24 @@ f32 func_8022A27C(f32 arg0, f32 arg1) {
         fabsA0 = temp_ft5;
         flag = 1;
     }
-    ratio = (fabsA0 / fabsA1);
+    temp_ft5 = (fabsA0 / fabsA1);
     absdiff = FABS(fabsA0 - fabsA1);
-    ret = (ratio * PW_PI_4) + (0.309f * absdiff * ratio);
-    pi2 = PW_PI_2;
+    ret = (PW_PI_4 * (fabsA0 / fabsA1)) + (0.309f * absdiff * temp_ft5);
+
+    temp_ft5 = 0; // fake store to prevent emitting additional mov instr.
     if (arg1 >= 0.0f) {
         if (flag) {
-            ret = pi2 - ret;
+            ret = PW_PI_2 - ret;
         }
     } else if (flag) {
-        ret = pi2 + ret;
+        ret = PW_PI_2 + ret;
     } else {
         ret = PW_PI - ret;
     }
+
     if (arg0 >= 0.0f) {
         return ret;
+    } else {
+        return -ret;
     }
-    return -ret;
 }
-#endif
-
