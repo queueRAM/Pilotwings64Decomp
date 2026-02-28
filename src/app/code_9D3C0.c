781df7061a688c8aa74064acb57420ac4eebd14b
queueRAM <queueRAM@proton.me>
2026-02-28 09:30:54 -0800

    Decompile func_803160B0

--- src/app/code_9D3C0.c
+++ src/app/code_9D3C0.c
@@ -1,8 +1,12 @@
 #include "common.h"
 #include <uv_graphics.h>
 #include <uv_level.h>
+#include "code_99D40.h"
 #include "credits.h"
+#include "menu.h"
 #include "save.h"
+
+// TODO: usage of [unk9C+1].debugFlag are likely incorrect way to get to 0x96 offset
 
 f32 D_8034F8D0[] = { 0.0f, 0.2f, 0.4f, 0.6f, 1.0f };
 s32 D_8034F8E4 = 0;
@@ -13,7 +17,8 @@ s32 D_8034F8F4 = 0;
 
 extern s16 D_8036C120;
 extern s32 D_8036C124;
-extern u8 D_8036C128;
+extern u8 D_8036C128; // flag to determine if game is complete
+extern s32 D_8036C130[4]; // menu items, at least 4
 
 // forward declarations
 void func_80315F0C(void);
@@ -73,7 +78,29 @@ void func_80315F0C(void) {
     D_8036C124 = 0;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/app/code_9D3C0/func_803160B0.s")
+void func_803160B0(void) {
+    s32 menuY;
+    s32 count;
+
+    saveFileLoad((s32) D_80362690->unk0[D_80362690->unk9C+1].debugFlag);
+    if (D_8036C128 != 0) {
+        D_8036C130[0] = 0xC2;  // "Check album"
+        D_8036C130[1] = 0xBE;  // "Sound"
+        D_8036C130[2] = 0x13;  // "Congratulations!"
+        D_8036C130[3] = 0x11C; // "Return"
+        count = 4;
+        menuY = 80;
+    } else {
+        D_8036C130[0] = 0xC2;  // "Check album"
+        D_8036C130[1] = 0xBE;  // "Sound"
+        D_8036C130[2] = 0x11C; // "Return"
+        count = 3;
+        menuY = 100;
+    }
+    menuCreateItems(0x50, menuY, 6, 1.0f, 1.0f, D_8036C130, count);
+    func_80312F5C(1, 0xFF, 0xFF, 0xFF);
+    func_80312F5C(0, 0xFF, 0xFF, 0);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_9D3C0/func_803161A8.s")
 
