f2c2f6d4ec60ed4cac1602ac3b71cfe3d4fed37d
queueRAM <queueRAM@proton.me>
2026-03-10 11:47:22 +0000

    PR feedback: early returns in ringsLoad

--- src/app/rings.c
+++ src/app/rings.c
@@ -52,87 +52,92 @@ void ringsLoad(void) {
     s32 j;
     u8 unk72;
 
-    if (D_80362690->unkA0 != 0) {
-        D_8036DA74 = levelDataGetRNGS(&D_8036DA70);
-        if (D_8036DA74 > ARRAY_COUNT(D_8036DA78)) {
-            _uvDebugPrintf("rings : too many rings defined in level [%d]\n", D_8036DA74);
-            D_8036DA74 = 0;
-            return;
+    if (D_80362690->unkA0 == 0) {
+        return;
+    }
+
+    D_8036DA74 = levelDataGetRNGS(&D_8036DA70);
+    if (D_8036DA74 > ARRAY_COUNT(D_8036DA78)) {
+        _uvDebugPrintf("rings : too many rings defined in level [%d]\n", D_8036DA74);
+        D_8036DA74 = 0;
+        return;
+    }
+
+    if (D_8036DA74 == 0) {
+        return;
+    }
+
+    uvLevelAppend(0xE);
+    for (i = 0; i < D_8036DA74; i++) {
+        rngs = &D_8036DA70[i];
+        ring = &D_8036DA78[i];
+        if (ring->unk1B4 != 0) {
+            continue;
         }
-        if (D_8036DA74 != 0) {
-            uvLevelAppend(0xE);
-            for (i = 0; i < D_8036DA74; i++) {
-                rngs = &D_8036DA70[i];
-                ring = &D_8036DA78[i];
-                if (ring->unk1B4 != 0) {
-                    continue;
-                }
 
-                if (ring->unk1B5 == 0) {
-                    ring->unk144 = rngs->unk71;
-                    if (rngs->unk72 > 3) {
-                        rngs->unk72 = 3;
-                    }
-                    unk72 = rngs->unk72;
-                    ring->unk147 = ring->unk146 = unk72;
-                    ring->unk184 = rngs->size;
-                    ring->unk1B7 = rngs->unk55;
-                    ring->unk149 = rngs->unk1D;
-                    if (ring->unk1B7 && (ring->unk149 > 0)) {
-                        ring->unk1B9 = 1;
-                    }
-                    ring->unk160 = (u8)rngs->unk34;
-                    for (j = 0; j < ring->unk149; j++) {
-                        ring->unk14C[j] = rngs->unk20[j];
-                    }
-
-                    for (j = 0; j < ring->unk160; j++) {
-                        ring->unk164[j] = rngs->unk38[j];
-                    }
-                    if ((ring->unk1B7 == 0) || (ring->unk149 != 0)) {
-                        ring->unk145 = 1;
-                    } else {
-                        ring->unk145 = 0;
-                    }
-                    ring->unk178 = rngs->unk4D;
-                    ring->unk17C = rngs->unk50;
-                    ring->unk180 = 0.0f;
-                    ring->unk148 = rngs->unk4C;
-                    ring->unk1AC = 1000000.0f;
-                    ring->unk1B0 = 1000000.0f;
-                    ring->unk1A8 = 0.0f;
-                    ring->unk1A4 = 0.0f;
-                    ring->unk188 = rngs->unk58;
-                    ring->unk18C = rngs->unk5C;
-                    ring->unk190 = rngs->unk60;
-                    ring->unk194 = rngs->unk64;
-                    ring->unk198 = rngs->unk68;
-                    ring->unk19C = rngs->unk6C;
-                    ring->unk1A0 = rngs->axis; // unk70
-                    ring->unk1BA = rngs->unk73;
-                    uvSprintf(ring->unk1BB, "%s", rngs->unk74);
-                    func_80313640(rngs->pos.x, rngs->pos.y, rngs->pos.z, rngs->angle.x * 0.0174533f, rngs->angle.y * 0.0174533f, rngs->angle.z * 0.0174533f,
-                                  &ring->unk4);
-                    uvMat4Copy(&ring->unk44, &ring->unk4);
-                    ring->unk44.m[3][0] = 0.0f;
-                    ring->unk44.m[3][1] = 0.0f;
-                    ring->unk44.m[3][2] = 0.0f;
-                    uvMat4Copy(&ring->unk84, &ring->unk4);
-                    ring->unk1B5 = 1;
-                }
-                if ((ring->unk1B7 != 0) && (ring->unk147 != 2)) {
-                    ring->unk1CA = hud_8031A6C8(rngs->pos.x, rngs->pos.y, rngs->pos.z);
-                }
-                func_803234A4(ring);
-                if ((ring->unk180 != 0) && (ring->unk1B7 != 0)) {
-                    uvDobjProps(ring->unk0, 4, (ring->unk144 == 0) ? 2 : 5, 0);
-                    uvDobjProps(ring->unk0, 4, (ring->unk144 == 0) ? 3 : 6, 0);
-                    func_80323720(ring);
-                }
+        if (ring->unk1B5 == 0) {
+            ring->unk144 = rngs->unk71;
+            if (rngs->unk72 > 3) {
+                rngs->unk72 = 3;
             }
-            func_80323364();
+            unk72 = rngs->unk72;
+            ring->unk147 = ring->unk146 = unk72;
+            ring->unk184 = rngs->size;
+            ring->unk1B7 = rngs->unk55;
+            ring->unk149 = rngs->unk1D;
+            if (ring->unk1B7 && (ring->unk149 > 0)) {
+                ring->unk1B9 = 1;
+            }
+            ring->unk160 = (u8)rngs->unk34;
+            for (j = 0; j < ring->unk149; j++) {
+                ring->unk14C[j] = rngs->unk20[j];
+            }
+
+            for (j = 0; j < ring->unk160; j++) {
+                ring->unk164[j] = rngs->unk38[j];
+            }
+            if ((ring->unk1B7 == 0) || (ring->unk149 != 0)) {
+                ring->unk145 = 1;
+            } else {
+                ring->unk145 = 0;
+            }
+            ring->unk178 = rngs->unk4D;
+            ring->unk17C = rngs->unk50;
+            ring->unk180 = 0.0f;
+            ring->unk148 = rngs->unk4C;
+            ring->unk1AC = 1000000.0f;
+            ring->unk1B0 = 1000000.0f;
+            ring->unk1A8 = 0.0f;
+            ring->unk1A4 = 0.0f;
+            ring->unk188 = rngs->unk58;
+            ring->unk18C = rngs->unk5C;
+            ring->unk190 = rngs->unk60;
+            ring->unk194 = rngs->unk64;
+            ring->unk198 = rngs->unk68;
+            ring->unk19C = rngs->unk6C;
+            ring->unk1A0 = rngs->axis; // unk70
+            ring->unk1BA = rngs->unk73;
+            uvSprintf(ring->unk1BB, "%s", rngs->unk74);
+            func_80313640(rngs->pos.x, rngs->pos.y, rngs->pos.z, rngs->angle.x * 0.0174533f, rngs->angle.y * 0.0174533f, rngs->angle.z * 0.0174533f,
+                          &ring->unk4);
+            uvMat4Copy(&ring->unk44, &ring->unk4);
+            ring->unk44.m[3][0] = 0.0f;
+            ring->unk44.m[3][1] = 0.0f;
+            ring->unk44.m[3][2] = 0.0f;
+            uvMat4Copy(&ring->unk84, &ring->unk4);
+            ring->unk1B5 = 1;
+        }
+        if ((ring->unk1B7 != 0) && (ring->unk147 != 2)) {
+            ring->unk1CA = hud_8031A6C8(rngs->pos.x, rngs->pos.y, rngs->pos.z);
+        }
+        func_803234A4(ring);
+        if ((ring->unk180 != 0) && (ring->unk1B7 != 0)) {
+            uvDobjProps(ring->unk0, 4, (ring->unk144 == 0) ? 2 : 5, 0);
+            uvDobjProps(ring->unk0, 4, (ring->unk144 == 0) ? 3 : 6, 0);
+            func_80323720(ring);
         }
     }
+    func_80323364();
 }
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/rings/func_80323DCC.s")
