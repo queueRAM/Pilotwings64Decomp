ff26f810033c1d1114b2bf59532afb2e320a9d7f
queueRAM <queueRAM@proton.me>
2026-03-26 22:59:58 -0700

    Decompile func_802F2110

--- src/app/code_78620.c
+++ src/app/code_78620.c
@@ -1,6 +1,16 @@
 #include "common.h"
+#include <uv_dobj.h>
 #include "code_78620.h"
 
+typedef struct {
+    s32 unk0;
+    f32 unk4;
+    u8 pad4[0x1C];
+} Unk80368010; // size = 0x24
+
+extern Unk80368010 D_80368010[];
+
+void func_802F10F0(s32);
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_78620/func_802F10F0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_78620/func_802F11F8.s")
@@ -23,4 +33,10 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_78620/func_802F205C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/app/code_78620/func_802F2110.s")
+void func_802F2110(s32 idx, f32 interval) {
+    if (D_80368010[idx].unk0 != 0xFFFF) {
+        uvDobjState(D_80368010[idx].unk0, 2);
+        D_80368010[idx].unk4 = interval * 360.0f;
+        func_802F10F0(idx);
+    }
+}
