33c6a7211cca3322520151549ccfb2376601e018
queueRAM <queueRAM@proton.me>
2026-03-26 23:08:45 -0700

    Decompile func_802F1FF0

--- src/app/code_78620.c
+++ src/app/code_78620.c
@@ -1,14 +1,17 @@
 #include "common.h"
 #include <uv_dobj.h>
 #include "code_78620.h"
+#include "proxanim.h"
 
 typedef struct {
-    s32 unk0;
-    f32 unk4;
-    u8 pad4[0x1C];
+    s32 objId;
+    f32 pathAngle;
+    u8 pad8[0x1C - 8];
+    s32 proxId;
+    u8 pad20[4];
 } Unk80368010; // size = 0x24
 
-extern Unk80368010 D_80368010[];
+extern Unk80368010 D_80368010[4];
 
 void func_802F10F0(s32);
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_78620/func_802F10F0.s")
@@ -29,16 +32,28 @@ void func_802F10F0(s32);
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_78620/func_802F1FA0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/app/code_78620/func_802F1FF0.s")
+void func_802F1FF0(void) {
+    Unk80368010* glider;
+    s32 i;
+
+    for (i = 0; i < ARRAY_COUNT(D_80368010); i++) {
+        glider = &D_80368010[i];
+        if (glider->objId != 0xFFFF) {
+            uvDobjModel(glider->objId, 0xFFFF);
+            glider->objId = 0xFFFF;
+            func_803212DC(glider->proxId);
+        }
+    }
+}
 
 void func_802F205C(s32 idx, s32* objId, f32* interval, Vec3F* pos) {
     Mtx4F pose;
     Unk80368010* glider;
 
     glider = &D_80368010[idx];
-    if (glider->unk0 != 0xFFFF) {
-        *objId = glider->unk0;
-        *interval = glider->unk4 / 360.0f;
+    if (glider->objId != 0xFFFF) {
+        *objId = glider->objId;
+        *interval = glider->pathAngle / 360.0f;
         uvDobjGetPosm(*objId, 0, &pose);
         pos->x = pose.m[3][0];
         pos->y = pose.m[3][1];
@@ -53,9 +68,9 @@ void func_802F205C(s32 idx, s32* objId, f32* interval, Vec3F* pos) {
 }
 
 void func_802F2110(s32 idx, f32 interval) {
-    if (D_80368010[idx].unk0 != 0xFFFF) {
-        uvDobjState(D_80368010[idx].unk0, 2);
-        D_80368010[idx].unk4 = interval * 360.0f;
+    if (D_80368010[idx].objId != 0xFFFF) {
+        uvDobjState(D_80368010[idx].objId, 2);
+        D_80368010[idx].pathAngle = interval * 360.0f;
         func_802F10F0(idx);
     }
 }
