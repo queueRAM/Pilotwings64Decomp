0f3a3a60ae0ac7c595f170e63891ac55c1df6650
queueRAM <queueram@proton.me>
2026-03-28 07:19:44 +0000

    Rename and decompile app/code_9C080 -> app/missi
    - Rename app/code_9C080 -> app/missi
    - Decomp func_803159F4
    - Decomp func_8031596C and add uv_seq.h from PR#105
    - Decomp func_80315734
    - Decomp func_8031565C
    - Decomp func_80315568
    - Decomp func_8031555C
    - Decomp func_80315550
    - Decomp func_80315508
    - Decomp func_803154C0
    - Decomp func_80315474
    - Decomp func_80315418
    - Decomp func_803153AC
    - Decomp func_80315394
    - Decomp func_8031531C
    - Decomp func_803151AC
    - Decomp func_80315028
    - Decomp func_80315028
    - Decomp func_80314EE8
    - Decomp func_80314CE8
    - Decomp func_80314B50
    - Decomp func_80314B5C (NON_MATCHING)
    - Map missi .bss
    - Rename missi functions and variables

---
+++ include/uv_seq.h
@@ -0,0 +1,20 @@
+#ifndef UV_SEQ_H
+#define UV_SEQ_H
+
+#include <PR/ultratypes.h>
+
+#define SEQ_PROP_1(x) 1, (x)
+#define SEQ_PROP_2(x) 2, (x)
+#define SEQ_PROP_3(x) 3, (x)
+#define SEQ_PROP_4(x) 4, (x)
+#define SEQ_PROP_5(x) 5, (x)
+#define SEQ_PROP_END 0
+
+void func_80218BA0(void);
+void func_80218CA4(void);
+void uvSeqModel(s32, s32);
+void uvSeqProps(s32, ...);
+s32 func_80218F88(void);
+s32 func_80219240(s32);
+
+#endif // UV_SEQ_H
