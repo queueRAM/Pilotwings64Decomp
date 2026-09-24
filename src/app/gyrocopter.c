5d582c148070246967643b570968a3c19963c94a
queueRAM <queueRAM@proton.me>
2026-09-24 06:19:09 +0000

    jp: C gyrocopter

--- src/app/gyrocopter.c
+++ src/app/gyrocopter.c
@@ -1489,9 +1489,11 @@ void func_80307EA8(GyrocopterData* gcData) {
 
     if (gcData->unk10.m[3][2] > 700.0f) {
         spD8 = 1.0f - ((gcData->unk10.m[3][2] - 700.0f) / 100.0f);
+#if defined(VERSION_US)
         if (spD8 < 0.0f) {
             spD8 = 0.0f;
         }
+#endif
     } else {
         spD8 = 1.0f;
     }
